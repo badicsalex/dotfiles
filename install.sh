2c5a183cd047dc410b5bf7e903ba09b95e5f45ca
Alex Badics <admin@stickman.hu>
2022-05-21 10:26:07 +0200

    nvim: import  LunarVim/Neovim-from-scratch as is
    Will be modified in the next commit

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ ln -sfn $SCRIPT_DIR/zsh/aliases ~/.aliases
 ln -sfn $SCRIPT_DIR/zsh/zshrc ~/.zshrc
 ln -sfn $SCRIPT_DIR/zsh/alex.zsh-theme ~/.oh-my-zsh/themes/alex.zsh-theme
 
-mkdir -p ~/.config/nvim/lua
+rm -rf ~/.config/nvim/lua
 mkdir -p ~/.config/nvim/colors
 mkdir -p ~/.config/nvim/syntax
 mkdir -p ~/.config/nvim/after/syntax
 
-ln -sfn $SCRIPT_DIR/nvim/init.vim  ~/.config/nvim/init.vim
-ln -sfn $SCRIPT_DIR/nvim/config.lua  ~/.config/nvim/lua/config.lua
+ln -sfn $SCRIPT_DIR/nvim/init.lua  ~/.config/nvim/init.lua
+ln -sfn $SCRIPT_DIR/nvim/lua  ~/.config/nvim/lua
 ln -sfn $SCRIPT_DIR/nvim/monokai_ad.vim ~/.config/nvim/colors/monokai_ad.vim
 ln -sfn $SCRIPT_DIR/nvim/syntax/tatsu.vim ~/.config/nvim/syntax/tatsu.vim
 ln -sfn $SCRIPT_DIR/nvim/syntax/c_after.vim ~/.config/nvim/after/syntax/c.vim
