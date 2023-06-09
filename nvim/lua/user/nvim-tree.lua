39af1693c6ad4edc9d51e95069727ceeeaabce12
Alex Badics <admin@stickman.hu>
2023-06-09 17:45:15 +0200

    nvim: fix nvim-tree autostart and autoquit

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -74,10 +74,36 @@ nvim_tree.setup {
   },
 }
 
-vim.cmd [[ autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() && len(filter(getbufinfo(), 'v:val.changed == 1')) == 0 | quit | endif ]]
+vim.api.nvim_create_autocmd({"QuitPre"}, {
+    callback = function()
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(invalid_win, w)
+          end
+        end
+        if #invalid_win == #wins - 1 then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+        end
+      end
+})
+
+--vim.cmd [[ autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() && len(filter(getbufinfo(), 'v:val.changed == 1')) == 0 | quit | endif ]]
 
 local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
+    require("nvim-tree.api").tree.open({focus=false, find_file=true})
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") == nil then
+          vim.api.nvim_set_current_win(w)
+          require('lualine').refresh()
+          return
+      end
+    end
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
